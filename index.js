b02f987582df99b6e70e0d1cce8563df5380fa4b
Kyle Nathan <kyle@wearelion.com>
2017-06-08 10:23:43 +0100

    Add ember-longleat-components.scss

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 module.exports = {
   name: 'ember-longleat-components',
 
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
+  },
+
   isDevelopingAddon: function() {
     return true;
   }
