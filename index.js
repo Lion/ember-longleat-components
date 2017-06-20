bfefc2200a4dd9c19db755d9041922f17d75234b
Kyle Nathan <kyle@wearelion.com>
2017-06-20 10:22:05 +0100

    Add and fix custom bourbon/neat installation

--- index.js
+++ index.js
@@ -4,8 +4,18 @@
 module.exports = {
   name: 'ember-longleat-components',
 
-  included: function(/* app */) {
+  included: function(app, parentAddon ) {
     this._super.included.apply(this, arguments);
+
+    var target = (parentAddon || app);
+    target.options.sassOptions = target.options.sassOptions || {};
+
+    target.options.sassOptions.includePaths = target.options.sassOptions.includePaths || [
+      'node_modules/bourbon/app/assets/stylesheets/',
+      'node_modules/bourbon-neat/core/',
+      'node_modules/bourbon-neat/contrib/'
+    ];
+
   },
 
   isDevelopingAddon: function() {
