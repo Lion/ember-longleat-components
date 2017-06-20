7f63cc955e23b10dc4e49e3fce6435e19f610ace
Kyle Nathan <kyle@wearelion.com>
2017-06-20 10:22:48 +0100

    Refactor sku-quantity-minus-plus component

--- addon/components/sku-quantity-minus-plus/component.js
+++ addon/components/sku-quantity-minus-plus/component.js
@@ -1,202 +1,133 @@
 import Ember from 'ember';
 import layout from './template';
+const { Component, computed, get, getProperties, inject, isEmpty } = Ember;
+const { service } = inject;
+const { alias, filter, gt, mapBy, not, notEmpty, sum } = computed;
 
-export default Ember.Component.extend({
+export default Component.extend({
+  skuService: service('sku'),
+  
   layout,
   
-  classNames: ['component-sku-quantity-minus-plus'],
-
+  classNames: ['sku-quantity-minus-plus'],
   defaultMax: 32,
+  incrementAmount: 1,
+  canIncrementForProduct: true,
 
-  // titleMeta: Ember.computed.filterBy('sku.metadata', 'metum.name', 'Title'),
-  // title: Ember.computed.alias('titleMeta.firstObject.value'),
+  hasTitleTemplate: notEmpty('skuTitleTemplate'),
+  hasAge: notEmpty('fieldsHash.age'),
+  hasQuantity: gt('quantity', 0),
+  fields: alias('sku.skuFields'),
+  isNotFree: gt('sku.price', 0),
+  isFree: not('isNotFree'),
+  skuBasketItemQuantities: mapBy('skuBasketItems', 'quantity'),
+  skuBasketItemQuantity: sum('skuBasketItemQuantities'),
+  cannotIncrement: not('canIncrement'),
+  canDecrement: gt('skuBasketItemQuantity', 0),
+  cannotDecrement: not('canDecrement'),
+  hasSkuMaxQuantity: gt('sku.maxQuantity', 0),
+  
+  skuBasketItems: filter('basketItems', function(basketItem) {
+    return get(basketItem, 'sku.id') === get(this, 'sku.id');
+  }),
 
-  // hasAge: Ember.computed.notEmpty('ageMeta'),
-  // ageMeta: Ember.computed.filterBy('sku.metadata', 'metum.name', 'Age'),
-  // age: Ember.computed.alias('ageMeta.firstObject.value'),
+  fieldsHash: computed(
+    'fields.[]',
+    function() {
+      const skuService = get(this, 'skuService');
+      const fields = get(this, 'fields');
+      return skuService.fieldsToHash(fields);
+    }
+  ),
 
-  // hasTitleTemplate: Ember.computed.notEmpty('titleTemplate'),
-  // renderedTitle: Ember.computed(
-  //   'titleTemplate',
-  //   'metaMap.[].name',
-  //   'metaMap.[].value',
-  //   function() {
-  //     return this.get('metaMap').reduce((title, field) => title.replace('{{' + field.name + '}}', field.value), this.get('titleTemplate'));
-  //   }
-  // ),
+  renderedTitle: computed(
+    'skuTitleTemplate',
+    'fieldsHash.[]',
+    function() {
+      let fieldsHash = get(this, 'fieldsHash');
+      let skuTitleTemplate = get(this, 'skuTitleTemplate');
+      return Object.keys(fieldsHash).reduce(
+        (title, key) => {
+          let regex = new RegExp(`{{${key}}}`, 'gi');
+          return title.replace(regex, fieldsHash[key])
+        }, 
+        skuTitleTemplate
+      );
+    }
+  ),
 
-  // metaMap: Ember.computed(
-  //   'sku.metadata.@each.metum',
-  //   'sku.metadata.@each.value',
-  //   function() {
-  //     return this.get('sku.metadata').reduce((metaMap, metadatum) => {
-  //       metaMap.push({
-  //         name: metadatum.get('metum.name'), 
-  //         value: metadatum.get('value')
-  //       });
-  //       return metaMap;
-  //     }, []);
-  //   }
-  // ),
+  lineTotal: computed(
+    'skuBasketItemQuantity',
+    'sku.price',
+    function() {
+      return (get(this, 'skuBasketItemQuantity') * get(this, 'sku.price')).toFixed(2);
+    }
+  ),
+  
+  available: computed(
+    'maxQuantity',
+    'skuBasketItemQuantity',
+    function() {
+      return get(this, 'maxQuantity') - get(this, 'skuBasketItemQuantity');
+    }
+  ),
 
+  canIncrement: computed(
+    'available',
+    'incrementAmount',
+    'canIncrementForProduct',
+    'isFree',
+    function() {
+      let {isNotFree, canIncrementForProduct, available, incrementAmount} = getProperties(this, 'isNotFree', 'canIncrementForProduct', 'available', 'incrementAmount');
+      if (isNotFree) {
+        return (canIncrementForProduct && (incrementAmount <= available));
+      }
+      return (incrementAmount <= available);
+    }
+  ),
 
-  // lineTotal: Ember.computed(
-  //   'skuBasketItemQuantity',
-  //   'sku.price',
-  //   function() {
-  //     return (this.get('skuBasketItemQuantity') * this.get('sku.price')).toFixed(2);
-  //   }
-  // ),
+  maxQuantity: computed(
+    'defaultMax',
+    'hasSkuMaxQuantity',
+    'sku.maxQuantity',
+    function() {
+      if (get(this, 'hasSkuMaxQuantity')) {
+        return get(this, 'sku.maxQuantity');
+      }
+      return get(this, 'defaultMax');
+    }
+  ),
 
-  // skuBasketItems: Ember.computed(
-  //   'basketItems.[]',
-  //   'basketItems.@each.session',
-  //   'sku',
-  //   'date',
-  //   'session.id',
-  //   function() {
-  //     let sku = this.get('sku');
-  //     let dateTimestamp = null;
-  //     if (Ember.isPresent(this.get('date')) &&
-  //       sku.get('product.requiresDate') === true
-  //     ) {
-  //       dateTimestamp = this.get('date').unix();
-  //     }
-  //     return this.get('basketItems').filter((basketItem) => {
-  //       return (
-  //         basketItem.get('sku.id') === sku.get('id') &&
-  //         basketItem.get('dateTimestamp') === dateTimestamp &&
-  //         basketItem.get('session.id') === this.get('session.id')
-  //       );
-  //     });
-  //   }
-  // ),
-  // skuBasketItemQuantities: Ember.computed.mapBy('skuBasketItems', 'quantity'),
-  // skuBasketItemQuantity: Ember.computed.sum('skuBasketItemQuantities'),
+  actions: {
 
-  // sessionBasketItems: Ember.computed(
-  //   'basketItems.@each.session.id',
-  //   'session.id',
-  //   function() {
-  //     return this.get('basketItems')
-  //     .filter(basketItem => (basketItem.get('session.id') === this.get('session.id')));
-  //   }
-  // ),
-  // sessionBasketQuantity: Ember.computed(
-  //   'sessionBasketItems.@each.quantity',
-  //   'sessionBasketItems.@each.sku',
-  //   function() {
-  //     return this.get('sessionBasketItems').reduce(
-  //       (quantity, basketItem) => 
-  //       quantity + basketItem.get('quantity') * (basketItem.get('sku.stockItems').reduce(
-  //         (stockItemQuantity, stockItem) => 
-  //         stockItemQuantity + stockItem.get('quantity'), 0)
-  //       ), 0
-  //     );
-  //   }
-  // ),
+    increment() {
+      const basketItems = get(this, 'basketItems');
+      let basketItem = get(this, 'skuBasketItems.firstObject');
+      let sku = get(this, 'sku');
 
-  // hasQuantity: Ember.computed.gt('quantity', 0),
-  // isNotFree: Ember.computed.gt('sku.price', 0),
-  // isFree: Ember.computed.not('isNotFree'),
+      if (!get(this, 'canIncrement')) {
+          return;
+      }
 
-  // available: Ember.computed(
-  //   'sessionBasketQuantity',
-  //   'stockItemQuantity',
-  //   'maxQuantity',
-  //   'skuBasketItemQuantity',
-  //   'sku.minAvailableSessionSpaces',
-  //   function() {
-  //     if (this.get('session.available') < this.get('sku.minAvailableSessionSpaces')) {
-  //       return 0;
-  //     }
-  //     if (Ember.isPresent(this.get('session'))) {
-  //       return this.get('maxQuantity') - this.get('sessionBasketQuantity');
-  //     } 
-  //     return this.get('maxQuantity') - (this.get('skuBasketItemQuantity') * this.get('skuStockItemQuantity'));
-  //   }
-  // ),
+      if (isEmpty(basketItem)) {
+        return this.createBasketItem(basketItems, sku, 1);
+      }
 
-  // canIncrementForProduct: true,
-  // canIncrement: Ember.computed(
-  //   'available',
-  //   'inventoryIncrementAmount',
-  //   'canIncrementForProduct',
-  //   'isFree',
-  //   function() {
-  //     let {isNotFree, canIncrementForProduct, available, inventoryIncrementAmount} = this.getProperties('isNotFree', 'canIncrementForProduct', 'available', 'inventoryIncrementAmount');
-  //     if (isNotFree) {
-  //       return (canIncrementForProduct && (inventoryIncrementAmount <= available));
-  //     }
-  //     return (inventoryIncrementAmount <= available);
-  //   }
-  // ),
-  // cannotIncrement: Ember.computed.not('canIncrement'),
-  // canDecrement: Ember.computed.gt('skuBasketItemQuantity', 0),
-  // cannotDecrement: Ember.computed.not('canDecrement'),
+      basketItem.incrementProperty('quantity');
+    },
 
-  // skuStockItemQuantities: Ember.computed.mapBy('sku.stockItems', 'quantity'),
-  // skuStockItemQuantity: Ember.computed.sum('skuStockItemQuantities'),
-  // inventoryIncrementAmount: Ember.computed.alias('skuStockItemQuantity'),
+    decrement() {
+      var basketItem = get(this, 'skuBasketItems.firstObject');
 
-  // hasSkuMaxQuantity: Ember.computed.gt('sku.maxQuantity', 0),
+      if (isEmpty(basketItem)) {
+        return;
+      }
 
-  // maxQuantity: Ember.computed(
-  //   'session.available',
-  //   'defaultMax',
-  //   'hasSkuMaxQuantity',
-  //   'sku.maxQuantity',
-  //   function() {
-  //     if (!Ember.isEmpty(this.get('session'))) {
-  //       return this.get('session.available');
-  //     }
-  //     if (this.get('hasSkuMaxQuantity')) {
-  //       return this.get('sku.maxQuantity');
-  //     }
-  //     return this.get('defaultMax');
-  //   }
-  // ),
+      basketItem.decrementProperty('quantity');
 
-  // actions: {
-
-  //   increment() {
-  //     let basketItem = this.get('skuBasketItems.firstObject');
-  //     let date = this.get('date');
-  //     let session = this.get('session');
-  //     let sku = this.get('sku');
-
-  //     if (!this.get('canIncrement')) {
-  //         return;
-  //     }
-
-  //     if (Ember.isEmpty(basketItem) || sku.get('useMetaFields') === 1) {
-  //       return this.createBasketItem({
-  //         basketItems: this.get('basketItems'),
-  //         date: date,
-  //         quantity: 1,
-  //         session: session,
-  //         sku: sku,
-  //         price: sku.get('price'),
-  //         isGift: this.get('isGift'),
-  //         giftMessage: this.get('giftMessage'),
-  //       });
-  //     }
-
-  //     basketItem.incrementProperty('quantity');
-  //   },
-
-  //   decrement() {
-  //     var basketItem = this.get('skuBasketItems.firstObject');
-
-  //     if (Ember.isEmpty(basketItem)) {
-  //       return;
-  //     }
-
-  //     basketItem.decrementProperty('quantity');
-
-  //     if (basketItem.get('quantity') === 0) {
-  //       return this.deleteBasketItem(this.get('basketItems'), basketItem);
-  //     }
-  //   }
-  // }
+      if (get(basketItem, 'quantity') === 0) {
+        return this.deleteBasketItem(get(this, 'basketItems'), basketItem);
+      }
+    }
+  }
 });
