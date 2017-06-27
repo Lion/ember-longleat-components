4f4193fb0c64025f3705e7cea8381d29282771a2
Kyle Nathan <kyle@wearelion.com>
2017-06-27 17:17:41 +0100

    Add ui-form-checkbox component

---
+++ addon/components/ui-form-checkbox/component.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+import layout from './template';
+const { Component, computed, get, isPresent } = Ember;
+const { alias, and, not, notEmpty, oneWay } = computed;
+
+export default Component.extend({
+  layout,
+  
+  classNameBindings: [
+    ':ui-form-checkbox',
+    'hasError',
+    'isValid'
+  ],
+
+  notValidating: not('validations.isValidating'),
+  didValidate: oneWay('targetObject.didValidate'),
+  hasContent: notEmpty('isChecked'),
+  isValid: and('hasContent', 'validations.isValid', 'notValidating'),
+  isInvalid: oneWay('validations.isInvalid'),
+  showErrorClass: and('notValidating', 'showMessage', 'hasContent', 'validations'),
+  hasError: alias('showMessage'),
+
+  showMessage: computed(
+    'validations.isDirty',
+    'isInvalid',
+    'didValidate',
+    function() {
+      return (get(this, 'validations.isDirty') || get(this, 'didValidate')) && get(this, 'isInvalid');
+    }
+  ),
+
+  actions: {
+    toggleValue() {
+      this.toggleProperty('isChecked');
+      if (isPresent(this.attrs.setValue)) {
+        this.setValue(get(this, 'isChecked'));
+      }
+    }
+  }
+});
