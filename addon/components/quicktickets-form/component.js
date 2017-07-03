7a99e9b0e3b0a01b116ac71475fe7091169cc909
Peter Laughton <peter@wearelion.com>
2017-07-03 18:43:14 +0100

    Add quicktickets-form

---
+++ addon/components/quicktickets-form/component.js
@@ -0,0 +1,61 @@
+import Ember from 'ember';
+import layout from './template';
+import moment from 'moment';
+
+const { Component, get, inject, set } = Ember;
+
+export default Ember.Component.extend({
+  layout, 
+  
+  classNameBindings: [
+    'isOpen'
+  ],
+
+  basketService: inject.service('basket'),
+  store: inject.service(),
+  //openingTimes: inject.service('openingTimes'),
+
+  basketItems: [],
+  isLoading: false,
+  isAddingToBasket: true, 
+
+  minDate: moment.utc('2017-02-01').startOf('month'),
+  maxDate: moment.utc('2018-01-07'),
+
+  init() {
+    this._super(...arguments);
+    this.set('basketItems', []);
+  },
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this.get('basketItems')
+    .filterBy('isNew')
+    .invoke('destroyRecord');
+  },
+
+  actions: {
+    
+    didChangeDate() {
+
+    },
+
+    addToBasket(basketItems) {
+      this.sendAction();    
+    },
+
+    createBasketItem(basketItems, sku, quantity) {
+      const basketService = get(this, 'basketService');
+      return basketService.createBasketItem(basketItems, sku, quantity);
+    },
+
+    destroyBasketItem(basketItems, basketItem) {
+      const basketService = get(this, 'basketService');
+      return basketService.destroyBasketItem(basketItems, basketItem);
+    }
+  },
+});
