2ca640163478a5a8b94e3bd25724e06318818c17
Kyle Nathan <kyle@wearelion.com>
2017-07-26 14:49:55 +0100

    Add default cardImage value to product card

--- addon/components/product-card/component.js
+++ addon/components/product-card/component.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 import layout from './template';
 
-const { Component } = Ember;
+const { Component, computed, get } = Ember;
+const { alias, sort } = computed;
 
 export default Component.extend({
   layout,
-  classNames: ['product-card']
+  classNames: ['product-card'],
+  productImageSorting: ['id:asc'],
+
+  productImages: alias("product.productImages"),
+  sortedProductImages: sort('productImages', 'productImageSorting'),
+
+  cardImage: computed(
+    "sortedProductImages.firstObject.originalUrl",
+    function() {
+      return get(this, "sortedProductImages.firstObject.originalUrl");
+    }
+  )
 });
