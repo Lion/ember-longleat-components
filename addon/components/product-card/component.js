261680e758d943bb51e88418b027e2a48e7ae1ea
Kyle Nathan <kyle@wearelion.com>
2017-07-27 10:48:01 +0100

    Remove cardImage update in product-card

--- addon/components/product-card/component.js
+++ addon/components/product-card/component.js
@@ -1,21 +1,10 @@
 import Ember from 'ember';
 import layout from './template';
 
-const { Component, computed, get } = Ember;
-const { alias, sort } = computed;
+const { Component } = Ember;
 
 export default Component.extend({
   layout,
   classNames: ['product-card'],
-  productImageSorting: ['id:asc'],
-
-  productImages: alias("product.productImages"),
-  sortedProductImages: sort('productImages', 'productImageSorting'),
-
-  cardImage: computed(
-    "sortedProductImages.firstObject.originalUrl",
-    function() {
-      return get(this, "sortedProductImages.firstObject.originalUrl");
-    }
-  )
+  
 });
