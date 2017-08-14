394840b567c8c83de86e56f5c7651dfb1b7d1ff7
Peter Laughton <peter@wearelion.com>
2017-08-14 11:08:38 +0100

    Add delivery option to checkout-progress-bar

--- addon/components/checkout-progress-bar/component.js
+++ addon/components/checkout-progress-bar/component.js
@@ -6,12 +6,14 @@ const { alias, equal, or } = computed;
 export default Component.extend({
   layout,
 
+  isDeliveryActive: equal('step', 'delivery'),
   isExtrasActive: equal('step', 'extras'),
   isDetailsActive: equal('step', 'details'),
   isPaymentActive: equal('step', 'payment'),
   isFinishActive: equal('step', 'finish'),
   isFinishDone: alias('isFinishActive'),
-
+  
+  isDeliveryDone: or('isDeliveryActive', 'isExtrasDone'),
   isExtrasDone: or('isDetailsActive', 'isDetailsDone'),
   isDetailsDone: or('isPaymentActive', 'isPaymentDone'),
   isPaymentDone: or('isFinishActive', 'isFinishDone'),
