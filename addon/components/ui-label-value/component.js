580fefe3d8a5bc0f906687168afff4e30b18d3a0
Kyle Nathan <kyle@wearelion.com>
2017-09-25 14:47:24 +0100

    Refactor ui-label-value

--- addon/components/ui-label-value/component.js
+++ addon/components/ui-label-value/component.js
@@ -1,6 +1,38 @@
 import Ember from 'ember';
 import layout from './template';
+const { Component, computed, get, isEmpty } = Ember;
 
-export default Ember.Component.extend({
-  layout
+let component = Component.extend({
+  layout,
+  classNames: ['component-ui-label-value'],
+
+  safeLabelWidth: computed(
+    'labelWidth',
+    function() {
+      if (isEmpty(get(this, 'labelWidth'))) {
+        return Ember.String.htmlSafe('');
+      }
+      return Ember.String.htmlSafe(
+        'width: ' + get(this, 'labelWidth') + ";"
+      );
+    }
+  ),
+
+  safeMarginLeft: computed(
+    'labelWidth',
+    function() {
+      if (isEmpty(get(this, 'labelWidth'))) {
+        return Ember.String.htmlSafe('');
+      }
+      return Ember.String.htmlSafe(
+        'margin-left: ' + get(this, 'labelWidth') + ";"
+      );
+    }
+  )
 });
+
+component.reopenClass({
+  positionalParams: ['label']
+});
+
+export default component;
