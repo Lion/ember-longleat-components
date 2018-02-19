8bd561c16cef56410826cf79fabaa67e1fecb77a
Kyle Nathan <kyle@wearelion.com>
2018-02-19 09:06:01 +0000

    Refactor apply-vouchercode-form

--- addon/components/apply-vouchercode-form/component.js
+++ addon/components/apply-vouchercode-form/component.js
@@ -1,29 +1,33 @@
-import Ember from 'ember';
-import layout from './template';
+import Ember from "ember";
+import layout from "./template";
 const { Component, computed, get, isBlank, set } = Ember;
 const { notEmpty } = computed;
 
 export default Component.extend({
   layout,
-  code: '',
+  code: "",
+  errors: null,
 
-  hasError: notEmpty('errorCode'),
+  hasError: notEmpty("errors"),
 
   actions: {
     submit() {
-      const code = get(this, 'code').trim();
+      const code = get(this, "code").trim();
+      let errors = [];
 
       if (isBlank(code)) {
-        set(this, 'errorCode', 'IS_EMPTY');
+        errors.pushObject({ title: "IS_EMPTY" });
+        set(this, "errors", errors);
         return;
       }
 
-      if (code.substring(0,2).toLowerCase() == 'cg') {
-        set(this, 'errorCode', 'IS_TESCO');
+      if (code.substring(0, 2).toLowerCase() == "cg") {
+        errors.pushObject({ title: "IS_TESCO" });
+        set(this, "errors", errors);
         return;
       }
 
-      this.applyVouchercode(code);
+      this.applyPromotion(code);
     }
   }
 });
