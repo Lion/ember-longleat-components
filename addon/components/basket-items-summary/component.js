f827d47f64693a2b8db7063dd5b1f39733970da1
Kyle Nathan <kyle@wearelion.com>
2018-02-19 09:06:43 +0000

    Refactor basket-items-summary for promotions

--- addon/components/basket-items-summary/component.js
+++ addon/components/basket-items-summary/component.js
@@ -1,50 +1,58 @@
-import Ember from 'ember';
-import layout from './template';
+import Ember from "ember";
+import layout from "./template";
 const { Component, isPresent, computed, get, set } = Ember;
-const { alias, and, empty, filter, filterBy, equal, not, notEmpty, or } = computed;
+const {
+  alias,
+  and,
+  empty,
+  filter,
+  filterBy,
+  equal,
+  not,
+  notEmpty,
+  or
+} = computed;
 
 export default Component.extend({
   layout,
 
   isVouchercodeFieldShowable: false,
 
-  isBasketEmpty: empty('basketItems'),
-  hasBasketItems: notEmpty('basketItems'),
-  isShowingRemoveVouchercode: alias('hasVouchercode'),
-  hasVouchercode: notEmpty('vouchercode.content'),
-  hasNoVouchercode: not('hasVouchercode'),
-  hasAPVoucherCode: equal('vouchercode.content.code', 'ANNUALPASSHOLDER'),
+  isBasketEmpty: empty("basketItems"),
+  hasBasketItems: notEmpty("basketItems"),
+  isShowingRemoveVouchercode: alias("hasVouchercode"),
+  hasVouchercode: notEmpty("promotionLines"),
+  hasNoVouchercode: not("hasVouchercode"),
+  hasAPVoucherCode: equal("vouchercode.content.code", "ANNUALPASSHOLDER"),
   visibleBasketItems: filterBy("basketItems", "isHidden", false),
   hasTescoTickets: notEmpty("tescoTickets"),
+  isShowingVouchercodeField: or("isVouchercodeFieldShowable", "hasVouchercode"),
 
   isShowingApplyVouchercode: and(
-    'isVouchercodeFieldShowable',
-    'hasNoVouchercode'
+    "isVouchercodeFieldShowable",
+    "hasNoVouchercode"
   ),
 
-  isShowingVouchercodeField: or(
-    'isVouchercodeFieldShowable',
-    'hasVouchercode'
-  ),
-  
   tescoTickets: filter("basketItems", function(basketItem) {
-    const isTescoField = get(basketItem, "sku.product.productFields")
-    .findBy("slug", "is-tesco");
+    const isTescoField = get(basketItem, "sku.product.productFields").findBy(
+      "slug",
+      "is-tesco"
+    );
 
-    return isPresent(isTescoField) && (get(isTescoField, "values") == true);
+    return isPresent(isTescoField) && get(isTescoField, "values") == true;
   }),
 
   init() {
     this._super(...arguments);
-    set(this, 'code', '');
-    if (isPresent(get(this, 'vouchercode'))) {
-      set(this, 'code', get(this, 'vouchercode.code'));
+    set(this, "code", "");
+    if (isPresent(get(this, "vouchercode"))) {
+      set(this, "code", get(this, "vouchercode.code"));
     }
   },
 
   actions: {
     showVouchercodeField() {
-      set(this, 'isVouchercodeFieldShowable', true);
-    },
+      set(this, "isVouchercodeFieldShowable", true);
+    }
   }
 });
