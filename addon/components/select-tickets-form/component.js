68fc048f4b39a14f54d6c8368b722316c3c89fa0
Kyle Nathan <kyle@wearelion.com>
2018-03-20 09:11:04 +0000

    Add sku min quantity support

--- addon/components/select-tickets-form/component.js
+++ addon/components/select-tickets-form/component.js
@@ -1,113 +1,143 @@
-import Ember from 'ember';
-import moment from 'moment';
-import layout from './template';
-const { Component, computed, get, getWithDefault, inject, isEmpty, isPresent, set, setProperties } = Ember;
+import Ember from "ember";
+import moment from "moment";
+import layout from "./template";
+const {
+  Component,
+  computed,
+  get,
+  getWithDefault,
+  inject,
+  isEmpty,
+  isPresent,
+  set,
+  setProperties
+} = Ember;
 const { service } = inject;
-const { alias, and, empty, equal, filter, filterBy, gt, mapBy, not, or, sum, uniq } = computed;
+const {
+  alias,
+  and,
+  empty,
+  equal,
+  filter,
+  filterBy,
+  gt,
+  mapBy,
+  not,
+  or,
+  sum,
+  uniq
+} = computed;
 
 export default Component.extend({
-
-  productService: service('product'),
+  productService: service("product"),
 
   layout,
 
-  classNames: ['select-tickets-form'],
+  classNames: ["select-tickets-form"],
 
   isShowingCalendar: false,
   isShowingVisitors: false,
   hasCondition: false,
   hasAcceptedCondition: false,
-  conditionMessage: 'I accept',
-  emptySelectVisitorsLabel: 'Number of visitors',
-  ticketHolderLabel: 'Visitor',
-  whosVisitingLabel: 'Who\'s visiting...',
-  
+  conditionMessage: "I accept",
+  emptySelectVisitorsLabel: "Number of visitors",
+  ticketHolderLabel: "Visitor",
+  whosVisitingLabel: "Who's visiting...",
+
   defaultProductMaxQuantity: 32,
   defaultProductMinQuantity: 0,
   isSubmitting: false,
 
-  date: moment.utc().startOf('day').add(1, 'day'),
-  minDate: moment.utc().startOf('month'),
+  date: moment
+    .utc()
+    .startOf("day")
+    .add(1, "day"),
+  minDate: moment.utc().startOf("month"),
   maxDate: moment.utc("2019-01-10"),
-  displayDate: moment.utc().startOf('month'),
+  displayDate: moment.utc().startOf("month"),
 
-  productFields: alias('product.productFields'),
-  quantities: mapBy('visibleBasketItems', 'quantity'),
+  productFields: alias("product.productFields"),
+  quantities: mapBy("visibleBasketItems", "quantity"),
   persistedProductQuantities: mapBy("persistedProductBasketItems", "quantity"),
   persistedProductQuantity: sum("persistedProductQuantities"),
-  firstSku: alias('product.skus.firstObject'),
-  productHasBookableDateField: filterBy('productFields', 'slug', 'has-bookable-date'),
-  requiresDate: equal('productHasBookableDateField.value', true),
+  firstSku: alias("product.skus.firstObject"),
+  productHasBookableDateField: filterBy(
+    "productFields",
+    "slug",
+    "has-bookable-date"
+  ),
+  requiresDate: equal("productHasBookableDateField.value", true),
   isHidingForm: and("hasNoProductQuantityAvailable", "isNotSubmitting"),
-  isShowingForm: not('isHidingForm'),
-  chooseSessions: and('product.hasSessions', 'isNotGift'),
-  isBasketItemsEmpty: empty('basketItems'),
-  totalVisitors: sum('quantities'),
-  hasBasketItems: gt('totalVisitors', 0),
-  pricedQuantities: mapBy('pricedBasketItems', 'quantity'),
-  totalPricedQuantity: sum('pricedQuantities'),
-  quantityIsValid: and('minQuantityIsValid', 'maxQuantityIsValid'),
-  canSubmit: and('hasBasketItems', 'quantityIsValid', 'conditionIsPassed'),
-  cannotSubmit: not('canSubmit'),
+  isShowingForm: not("isHidingForm"),
+  chooseSessions: and("product.hasSessions", "isNotGift"),
+  isBasketItemsEmpty: empty("basketItems"),
+  totalVisitors: sum("quantities"),
+  hasBasketItems: gt("totalVisitors", 0),
+  pricedQuantities: mapBy("pricedBasketItems", "quantity"),
+  totalPricedQuantity: sum("pricedQuantities"),
+  quantityIsValid: and("minQuantityIsValid", "maxQuantityIsValid"),
+  canSubmit: and("hasBasketItems", "quantityIsValid", "conditionIsPassed"),
+  cannotSubmit: not("canSubmit"),
   hasProductQuantityAvailable: gt("availableProductQuantity", 0),
   hasNoProductQuantityAvailable: not("hasProductQuantityAvailable", 0),
   isNotSubmitting: not("isSubmitting"),
 
-  hasMinQuantity: gt('productMinQuantity', 0),
-  hasMaxQuantity: gt('productMaxQuantity', 0),
-  hasSmallPrint: or('hasMinQuantity', 'hasMaxQuantity'),
-  skuFieldArrays: mapBy('skusWithStock', 'skuFields'),
-  skuDateFields: filterBy('skuFields', 'slug', 'bookable-date'),
-  skuDateValueArrays: mapBy('skuDateFields', 'values'),
-  dates: uniq('skuValueDates'),
+  hasMinQuantity: gt("productMinQuantity", 0),
+  hasMaxQuantity: gt("productMaxQuantity", 0),
+  hasSmallPrint: or("hasMinQuantity", "hasMaxQuantity"),
+  skuFieldArrays: mapBy("skusWithStock", "skuFields"),
+  skuDateFields: filterBy("skuFields", "slug", "bookable-date"),
+  skuDateValueArrays: mapBy("skuDateFields", "values"),
+  dates: uniq("skuValueDates"),
   visibleBasketItems: filterBy("basketItems", "isHidden", false),
-  skusWithStock: filter('skus', function(sku) {
-    if (get(sku, 'stockQuantity') > 0) {
+  skusWithStock: filter("skus", function(sku) {
+    if (get(sku, "stockQuantity") > 0) {
       return sku;
-    } 
+    }
   }),
 
-  persistedProductBasketItems: filter(
-    'persistedBasketItems',
-    function(persistedBasketItem) {
-      return get(persistedBasketItem, "sku.product.id") === get(this, "product.id");
-    }
-  ),
+  persistedProductBasketItems: filter("persistedBasketItems", function(
+    persistedBasketItem
+  ) {
+    return (
+      get(persistedBasketItem, "sku.product.id") === get(this, "product.id")
+    );
+  }),
 
   productMaxQuantity: computed(
-    'productFieldsHash',
-    'defaultProductMaxQuantity',
+    "productFieldsHash",
+    "defaultProductMaxQuantity",
     function() {
-      let productFields = get(this, 'productFieldsHash');
+      let productFields = get(this, "productFieldsHash");
 
-      if (productFields['max-quantity']) {
-        return productFields['max-quantity']
+      if (productFields["max-quantity"]) {
+        return productFields["max-quantity"];
       } else {
-        return get(this, 'defaultProductMaxQuantity');
+        return get(this, "defaultProductMaxQuantity");
       }
     }
   ),
 
   productMinQuantity: computed(
-    'productFieldsHash',
-    'defaultProductMinQuantity',
+    "productFieldsHash",
+    "defaultProductMinQuantity",
     function() {
-      let productFields = get(this, 'productFieldsHash');
+      let productFields = get(this, "productFieldsHash");
 
-      if (productFields['min-quantity']) {
-        return productFields['min-quantity']
+      if (productFields["min-quantity"]) {
+        return productFields["min-quantity"];
       } else {
-        return get(this, 'defaultProductMinQuantity');
+        return get(this, "defaultProductMinQuantity");
       }
     }
   ),
 
   conditionIsPassed: computed(
-    'hasCondition',
-    'hasAcceptedCondition',
+    "hasCondition",
+    "hasAcceptedCondition",
     function() {
-      const hasCondition = get(this,'hasCondition');
-      const hasAcceptedCondition = get(this,'hasAcceptedCondition');
+      const hasCondition = get(this, "hasCondition");
+      const hasAcceptedCondition = get(this, "hasAcceptedCondition");
       if (hasCondition) {
         return hasAcceptedCondition;
       }
@@ -115,36 +145,33 @@ export default Component.extend({
     }
   ),
 
-  productFieldsHash: computed(
-    'productFields.[]',
-    function() {
-      const productService = get(this, 'productService');
-      const productFields = get(this, 'productFields');
-      return productService.fieldsToHash(productFields);
-    }
-  ),
+  productFieldsHash: computed("productFields.[]", function() {
+    const productService = get(this, "productService");
+    const productFields = get(this, "productFields");
+    return productService.fieldsToHash(productFields);
+  }),
 
-  pricedBasketItems: filter(
-    'basketItems',
-    function(basketItem) {
-      return get(basketItem, 'price') > 0;
-    }
-  ),
+  pricedBasketItems: filter("basketItems", function(basketItem) {
+    return get(basketItem, "price") > 0;
+  }),
 
   minQuantityIsValid: computed(
-    'totalPricedQuantity',
-    'productMinQuantity',
+    "totalPricedQuantity",
+    "productMinQuantity",
     function() {
-      return get(this, 'totalPricedQuantity') >= getWithDefault(this, 'productMinQuantity', 0);
+      return (
+        get(this, "totalPricedQuantity") >=
+        getWithDefault(this, "productMinQuantity", 0)
+      );
     }
   ),
 
   maxQuantityIsValid: computed(
-    'totalPricedQuantity',
-    'productMaxQuantity',
+    "totalPricedQuantity",
+    "productMaxQuantity",
     function() {
-      const maxQuantity = getWithDefault(this, 'productMaxQuantity', 0);
-      const totalPricedQuantity = get(this, 'totalPricedQuantity');
+      const maxQuantity = getWithDefault(this, "productMaxQuantity", 0);
+      const totalPricedQuantity = get(this, "totalPricedQuantity");
       if (maxQuantity === 0) {
         return true;
       }
@@ -156,18 +183,20 @@ export default Component.extend({
     "productMaxQuantity",
     "persistedProductQuantity",
     function() {
-      return get(this, 'productMaxQuantity') - get(this, "persistedProductQuantity")
+      return (
+        get(this, "productMaxQuantity") - get(this, "persistedProductQuantity")
+      );
     }
   ),
 
   canIncrementForProduct: computed(
-    'productMaxQuantity',
-    'totalPricedQuantity',
-    'availableProductQuantity',
+    "productMaxQuantity",
+    "totalPricedQuantity",
+    "availableProductQuantity",
     function() {
       const availableProductQuantity = get(this, "availableProductQuantity");
-      const maxQuantity = get(this, 'productMaxQuantity');
-      const totalPricedQuantity = get(this, 'totalPricedQuantity')
+      const maxQuantity = get(this, "productMaxQuantity");
+      const totalPricedQuantity = get(this, "totalPricedQuantity");
       if (isEmpty(maxQuantity) || maxQuantity === 0) {
         return true;
       }
@@ -175,120 +204,112 @@ export default Component.extend({
     }
   ),
 
-  totalPrice: computed(
-    'basketItems.@each.{price,quantity}',
-    function() {
-      const basketItems = get(this, 'basketItems');
-      return basketItems.reduce((total, basketItem) => 
-        total + (get(basketItem, 'price') * get(basketItem, 'quantity'))
-      , 0)
+  totalPrice: computed("basketItems.@each.{price,quantity}", function() {
+    const basketItems = get(this, "basketItems");
+    return basketItems
+      .reduce(
+        (total, basketItem) =>
+          total + get(basketItem, "price") * get(basketItem, "quantity"),
+        0
+      )
       .toFixed(2);
-    }
-  ),
+  }),
 
-  dateLabel: computed(
-    'date',
-    function() {
-      const date = get(this, 'date');
-      if (isEmpty(date)) {
-        return "Choose date";
-      }
-      return moment.utc(date).format('DD/MM/YYYY');
+  dateLabel: computed("date", function() {
+    const date = get(this, "date");
+    if (isEmpty(date)) {
+      return "Choose date";
     }
-  ),
+    return moment.utc(date).format("DD/MM/YYYY");
+  }),
 
   selectVisitorsLabel: computed(
-    'isBasketItemsEmpty',
-    'totalVisitors',
-    'basketItems.[]',
-    'emptySelectVisitorsLabel',
-    'ticketHolderLabel',
+    "isBasketItemsEmpty",
+    "totalVisitors",
+    "basketItems.[]",
+    "emptySelectVisitorsLabel",
+    "ticketHolderLabel",
     function() {
-      const totalVisitors = get(this, 'totalVisitors');
-      const isBasketItemsEmpty = get(this, 'isBasketItemsEmpty');
-      const emptyLabel = get(this, 'emptySelectVisitorsLabel');
-      const ticketHolderLabel = get(this, 'ticketHolderLabel');
+      const totalVisitors = get(this, "totalVisitors");
+      const isBasketItemsEmpty = get(this, "isBasketItemsEmpty");
+      const emptyLabel = get(this, "emptySelectVisitorsLabel");
+      const ticketHolderLabel = get(this, "ticketHolderLabel");
       if (isBasketItemsEmpty) {
         return emptyLabel;
       }
       if (totalVisitors === 1) {
-        return totalVisitors + ' ' + ticketHolderLabel;
+        return totalVisitors + " " + ticketHolderLabel;
       }
-      return totalVisitors + ' ' + ticketHolderLabel +'s';
+      return totalVisitors + " " + ticketHolderLabel + "s";
     }
   ),
 
-  isSelectVisitorsDisabled: computed(
-    'requiresDate',
-    'date',
-    function() {
-      const requiresDate = get(this, 'requiresDate');
-      const date = get(this, 'date');
-      if (requiresDate) {
-        return isEmpty(date);
+  isSelectVisitorsDisabled: computed("requiresDate", "date", function() {
+    const requiresDate = get(this, "requiresDate");
+    const date = get(this, "date");
+    if (requiresDate) {
+      return isEmpty(date);
+    }
+    return false;
+  }),
+
+  skuFields: computed("skuFieldArrays.[]", function() {
+    let skuFieldArrays = get(this, "skuFieldArrays");
+    return skuFieldArrays.reduce(
+      (skuFields, skuFieldArray) =>
+        skuFields.pushObjects(skuFieldArray.toArray()),
+      []
+    );
+  }),
+
+  skuValueDates: computed("skuDateValueArrays.[]", function() {
+    let skuDateValueArrays = get(this, "skuDateValueArrays");
+    return skuDateValueArrays.reduce(
+      (dates, skuDateValueArray) => dates.pushObjects(skuDateValueArray),
+      []
+    );
+  }),
+
+  days: computed("displayDate", "dates.[]", function() {
+    let dates = get(this, "dates");
+    let startDay = get(this, "displayDate")
+      .clone()
+      .startOf("month")
+      .weekday(0);
+    let finishDay = get(this, "displayDate")
+      .clone()
+      .endOf("month")
+      .weekday(6);
+    let days = [];
+    while (startDay.isBefore(finishDay)) {
+      let key = "";
+      let isSelectable = false;
+
+      if (dates.includes(startDay.format())) {
+        isSelectable = true;
       }
-      return false;
-    }
-  ),
 
-  skuFields: computed(
-    'skuFieldArrays.[]',
-    function() {
-      let skuFieldArrays = get(this, 'skuFieldArrays')
-      return skuFieldArrays.reduce((skuFields, skuFieldArray) =>  skuFields.pushObjects(skuFieldArray.toArray()), []);
+      days.pushObject({
+        day: startDay.clone(),
+        name: startDay.format("YYYY-MM-DD"),
+        isSelectable: isSelectable,
+        isDisabled: !isSelectable,
+        key: key
+      });
+      startDay.add(1, "day");
     }
-  ),
+    return days;
+  }),
 
-  skuValueDates: computed(
-    'skuDateValueArrays.[]',
-    function() {
-      let skuDateValueArrays = get(this, 'skuDateValueArrays');
-      return skuDateValueArrays.reduce((dates, skuDateValueArray) => dates.pushObjects(skuDateValueArray), []);
-    }
-  ),
-  
-  days: computed(
-    'displayDate',
-    'dates.[]',
-    function() {
-      let dates = get(this, 'dates');
-      let startDay = get(this, 'displayDate').clone().startOf('month').weekday(0);
-      let finishDay = get(this, 'displayDate').clone().endOf('month').weekday(6);
-      let days = [];
-      while (startDay.isBefore(finishDay)) {
-        let key = "";
-        let isSelectable = false;
-        
-        if (dates.includes(startDay.format())) {
-          isSelectable = true;
-        }
-
-        days.pushObject({
-          day: startDay.clone(),
-          name: startDay.format('YYYY-MM-DD'),
-          isSelectable: isSelectable,
-          isDisabled: !isSelectable,
-          key: key
-        });
-        startDay.add(1, 'day');
-      }
-      return days;
-    }
-  ),
-
-  filteredSkus: computed(
-    'skus.[]',
-    'date',
-    function() {
-      const skus = get(this, 'skus');
-      const date = get(this, 'date').format();
-      return skus.filter(sku => 
-        get(sku, 'skuFields')
-        .filterBy('slug', 'bookable-date')
-        .findBy('values.firstObject', date)
-      );
-    }
-  ),
+  filteredSkus: computed("skus.[]", "date", function() {
+    const skus = get(this, "skus");
+    const date = get(this, "date").format();
+    return skus.filter(sku =>
+      get(sku, "skuFields")
+        .filterBy("slug", "bookable-date")
+        .findBy("values.firstObject", date)
+    );
+  }),
 
   didInsertElement() {
     const displayDate = get(this, "displayDate");
@@ -298,13 +319,13 @@ export default Component.extend({
   actions: {
     changeDisplayDate(displayDate) {
       this.changeDisplayDate(displayDate);
-      set(this, 'displayDate', displayDate);
+      set(this, "displayDate", displayDate);
     },
 
     selectDate(date) {
-      let basketItems = get(this, 'basketItems');
-      if (!date.isSame(get(this, 'date'))) {
-        basketItems.invoke('destroyRecord');
+      let basketItems = get(this, "basketItems");
+      if (!date.isSame(get(this, "date"))) {
+        basketItems.invoke("destroyRecord");
         basketItems.clear();
       }
 
@@ -319,8 +340,7 @@ export default Component.extend({
     },
 
     selectVisitors() {
-      set(this, 'isShowingVisitors', false);
+      set(this, "isShowingVisitors", false);
     }
-  },
-
+  }
 });
--- addon/components/sku-quantity-minus-plus/component.js
+++ addon/components/sku-quantity-minus-plus/component.js
@@ -1,105 +1,121 @@
-import Ember from 'ember';
-import layout from './template';
-const { Component, computed, get, getProperties, inject, isEmpty } = Ember;
+import Ember from "ember";
+import layout from "./template";
+const { Component, computed, get, getProperties, inject, isEmpty, set } = Ember;
 const { service } = inject;
 const { alias, filter, gt, mapBy, not, notEmpty, sum } = computed;
 
 export default Component.extend({
-  skuService: service('sku'),
-  
+  skuService: service("sku"),
+
   layout,
-  
-  classNames: ['sku-quantity-minus-plus'],
+
+  classNames: ["sku-quantity-minus-plus"],
   defaultMax: 32,
   incrementAmount: 1,
+  skuMinQuantity: 1,
   canIncrementForProduct: true,
 
-  hasAge: notEmpty('fieldsHash.age'),
-  hasQuantity: gt('quantity', 0),
-  fields: alias('sku.skuFields'),
-  isNotFree: gt('sku.price', 0),
-  isFree: not('isNotFree'),
+  hasAge: notEmpty("fieldsHash.age"),
+  hasQuantity: gt("quantity", 0),
+  fields: alias("sku.skuFields"),
+  isNotFree: gt("sku.price", 0),
+  isFree: not("isNotFree"),
   firstSkuBasketItem: alias("skuBasketItems.firstObject"),
-  skuBasketItemQuantities: mapBy('skuBasketItems', 'quantity'),
-  skuBasketItemQuantity: sum('skuBasketItemQuantities'),
-  cannotIncrement: not('canIncrement'),
-  canDecrement: gt('skuBasketItemQuantity', 0),
-  cannotDecrement: not('canDecrement'),
-  hasProductMaxQuantity: gt('sku.product.maxQuantity', 0),
+  skuBasketItemQuantities: mapBy("skuBasketItems", "quantity"),
+  skuBasketItemQuantity: sum("skuBasketItemQuantities"),
+  cannotIncrement: not("canIncrement"),
+  canDecrement: gt("skuBasketItemQuantity", 0),
+  cannotDecrement: not("canDecrement"),
+  hasProductMaxQuantity: gt("sku.product.maxQuantity", 0),
   bundleUUID: alias("firstSkuBasketItem.metadata.bundleUUID"),
   hasBundleUUID: notEmpty("bundleUUID"),
-  
-  skuBasketItems: filter('basketItems', function(basketItem) {
-    return get(basketItem, 'sku.id') === get(this, 'sku.id');
+
+  skuBasketItems: filter("basketItems", function(basketItem) {
+    return get(basketItem, "sku.id") === get(this, "sku.id");
   }),
 
   productMaxQuantity: computed(
-    'hasProductMaxQuantity',
-    'sku.product.maxQuantity',
-    'defaultMax',
+    "hasProductMaxQuantity",
+    "sku.product.maxQuantity",
+    "defaultMax",
     function() {
-      if (get(this, 'hasProductMaxQuantity')) {
-        return  get(this, 'sku.product.maxQuantity');
+      if (get(this, "hasProductMaxQuantity")) {
+        return get(this, "sku.product.maxQuantity");
       }
-      return get(this, 'defaultMax');
-    }
-  ),
-  
-  fieldsHash: computed(
-    'fields.[]',
-    function() {
-      const skuService = get(this, 'skuService');
-      const fields = get(this, 'fields');
-      return skuService.fieldsToHash(fields);
+      return get(this, "defaultMax");
     }
   ),
 
+  fieldsHash: computed("fields.[]", function() {
+    const skuService = get(this, "skuService");
+    const fields = get(this, "fields");
+    return skuService.fieldsToHash(fields);
+  }),
+
   lineTotal: computed(
-    'skuBasketItemQuantity',
-    'sku.price',
+    "skuBasketItemQuantity",
+    "sku.price",
     "bundledBasketItems.@each.price",
     function() {
-      let totalPrice = get(this, "bundledBasketItems").reduce((totalPrice, basketItem) => totalPrice + get(basketItem, "sku.price"), 0) + get(this, "sku.price");
-      return (get(this, 'skuBasketItemQuantity') * totalPrice);
+      let totalPrice =
+        get(this, "bundledBasketItems").reduce(
+          (totalPrice, basketItem) => totalPrice + get(basketItem, "sku.price"),
+          0
+        ) + get(this, "sku.price");
+      return get(this, "skuBasketItemQuantity") * totalPrice;
     }
   ),
 
   canIncrement: computed(
-    'available',
-    'incrementAmount',
-    'canIncrementForProduct',
-    'isFree',
+    "available",
+    "incrementAmount",
+    "canIncrementForProduct",
+    "isFree",
     function() {
-      let {isNotFree, canIncrementForProduct, available, incrementAmount} = getProperties(this, 'isNotFree', 'canIncrementForProduct', 'available', 'incrementAmount');
+      let {
+        isNotFree,
+        canIncrementForProduct,
+        available,
+        incrementAmount
+      } = getProperties(
+        this,
+        "isNotFree",
+        "canIncrementForProduct",
+        "available",
+        "incrementAmount"
+      );
 
       if (isNotFree) {
-        return (canIncrementForProduct && (incrementAmount <= available));
+        return canIncrementForProduct && incrementAmount <= available;
       }
-      return (incrementAmount <= available);
+      return incrementAmount <= available;
     }
   ),
 
-  available: computed(
-    'maxQuantity',
-    'skuBasketItemQuantity',
-    function() {
-      return get(this, 'maxQuantity') - get(this, 'skuBasketItemQuantity');
-    }
-  ),
+  available: computed("maxQuantity", "skuBasketItemQuantity", function() {
+    return get(this, "maxQuantity") - get(this, "skuBasketItemQuantity");
+  }),
 
   maxQuantity: computed(
-    'defaultMax',
-    'hasProductMaxQuantity',
-    'productMaxQuantity',
-    'sku.stockQuantity',
+    "defaultMax",
+    "hasProductMaxQuantity",
+    "productMaxQuantity",
+    "sku.stockQuantity",
     function() {
-      let fieldsHash = get(this, 'fieldsHash');
-      let skuMaxQuantity = get(fieldsHash, 'max-quantity');
+      let fieldsHash = get(this, "fieldsHash");
+      let skuMaxQuantity = get(fieldsHash, "max-quantity");
 
       if (skuMaxQuantity > 0) {
-        return Math.min(get(this, 'productMaxQuantity'), get(this, 'sku.stockQuantity'), skuMaxQuantity);
+        return Math.min(
+          get(this, "productMaxQuantity"),
+          get(this, "sku.stockQuantity"),
+          skuMaxQuantity
+        );
       } else {
-        return Math.min(get(this, 'productMaxQuantity'), get(this, 'sku.stockQuantity'));
+        return Math.min(
+          get(this, "productMaxQuantity"),
+          get(this, "sku.stockQuantity")
+        );
       }
     }
   ),
@@ -125,36 +141,45 @@ export default Component.extend({
   ),
 
   actions: {
-
     increment() {
-      const basketItems = get(this, 'basketItems');
-      let basketItem = get(this, 'firstSkuBasketItem');
-      let sku = get(this, 'sku');
-      
-      if (!get(this, 'canIncrement')) {
-          return;
+      const basketItems = get(this, "basketItems");
+      let basketItem = get(this, "firstSkuBasketItem");
+      let sku = get(this, "sku");
+      const quantity = get(basketItem, "quantity");
+      const minQuantity = get(this, "skuMinQuantity");
+
+      if (!get(this, "canIncrement")) {
+        return;
       }
 
       if (isEmpty(basketItem)) {
         return this.createBasketItem(basketItems, sku, 1);
       }
-      
-      basketItem.incrementProperty('quantity');
+
+      set(basketItem, "quantity", Math.max(quantity + 1, minQuantity));
+
       get(this, "bundledBasketItems").invoke("incrementProperty", "quantity");
     },
 
     decrement() {
-      var basketItem = get(this, 'firstSkuBasketItem');
+      var basketItem = get(this, "firstSkuBasketItem");
+      const quantity = get(basketItem, "quantity");
+      const minQuantity = get(this, "skuMinQuantity");
 
       if (isEmpty(basketItem)) {
         return;
       }
 
-      basketItem.decrementProperty('quantity');
+      if (quantity - 1 < minQuantity) {
+        set(basketItem, "quantity", 0);
+      } else {
+        basketItem.decrementProperty("quantity");
+      }
+
       get(this, "bundledBasketItems").invoke("decrementProperty", "quantity");
 
-      if (get(basketItem, 'quantity') === 0) {
-        return this.deleteBasketItem(get(this, 'basketItems'), basketItem);
+      if (get(basketItem, "quantity") === 0) {
+        return this.deleteBasketItem(get(this, "basketItems"), basketItem);
       }
     }
   }
